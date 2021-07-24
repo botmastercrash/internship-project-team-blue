91fca96be0a62548e472d638677e6451b53f1aac
IVBarzev19 <IVBarzev19@codingburgas.bg>
2021-07-24 20:04:20 +0300

    Fix ozone.py spider

--- scrapyProject/scrapyProject/spiders/ozone.py
+++ scrapyProject/scrapyProject/spiders/ozone.py
@@ -1,5 +1,5 @@
 import scrapy
-from scrapyProject.items import scrapyProjectItem
+from scrapyProject.items import ScrapyprojectItem
 
 class OzoneSpider(scrapy.Spider):
     name = 'ozone'
@@ -7,9 +7,9 @@ class OzoneSpider(scrapy.Spider):
     start_urls = ['https://www.ozone.bg/gaming/console-and-accessories/']
 
     def extractInfo(self, response):
-        item = scrapyProjectItem()
+ #      item = ScrapyprojectItem()
         item = {
-            "name": response.xpath(''' //*[@id="products-list"]/div/a/span[3]/span/text() ''')
+            "name": response.xpath(''' //*[@id="products-list"]/div[2]/a/span[3]/span/text() ''').getall()
         }
 
         yield item
